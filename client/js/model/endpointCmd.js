3d29f4dbb34efeb747ee66e06d0735152a59cdfa
stefinfection <sjeangeo@gmail.com>
2018-08-31 16:33:57 -0600

    Got bcftools filtering working, commented out for now until obtain appropriate cutoff metrics

--- client/js/model/endpointCmd.js
+++ client/js/model/endpointCmd.js
@@ -22,7 +22,7 @@ EndpointCmd.prototype.getVcfHeader = function (vcfUrl, tbiUrl) {
     return cmd;
 }
 
-EndpointCmd.prototype.getChromosomesFromVcf = function(vcfUrl, tbiUrl) {
+EndpointCmd.prototype.getChromosomesFromVcf = function (vcfUrl, tbiUrl) {
     var me = this;
     var args = ['-l', '"' + vcfUrl + '"'];
     if (tbiUrl) {
@@ -95,12 +95,24 @@ EndpointCmd.prototype.annotateEnrichment = function (vcfSource, refName, regions
         return null;
     }
 
-    // Filter vcf for probands only
+    // // Filter vcf for probands only
     if (controlSampleNames && controlSampleNames.length > 0) {
         let formattedControls = controlSampleNames.join("\n");
         let sampleNameFile = new Blob([formattedControls]);
         cmd = cmd.pipe(self.IOBIO.vt, ["subset", "-s", sampleNameFile, '-'], {ssl: self.useSSL});
     }
+
+    // Only use variants that have passed all Simons filters
+    cmd = cmd.pipe(self.IOBIO.vt, ['filter', '-f', 'PASS', '-t', '\"SSC_PASS\"', '-d', '\"Variants passing all SSC filters\"', '-'], {ssl: self.useSSL});
+
+    // Filter out alleles with <25% frequency - TODO: don't use info field, calculate this on a per sample basis and use some percentage cutoff
+    //cmd = cmd.pipe(self.IOBIO.bcftools, ['filter', '-i', '\"AVG(INFO/AF)>0.25\"'], {ssl: self.useSSL});
+
+    // Filter alleles with coverage <20x
+    // TODO: do this on a per variant basis also
+
+    // Filter alleles without representation on both strands
+    // TODO: do this on a per variant basis also
 
     // Annotate enrichment info
     cmd = cmd.pipe(self.IOBIO.gtEnricher, expSampleNames, {ssl: self.useSSL});
